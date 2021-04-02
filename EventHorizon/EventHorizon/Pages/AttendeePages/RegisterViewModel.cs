696ac95a7228d5e0dfa99b00e8d0a015033df902
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-04-02 09:18:02 +0200

    Added register base class.

---
+++ EventHorizon/EventHorizon/Pages/AttendeePages/RegisterViewModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EventHorizon.Pages.AttendeePages
+{
+    public class RegisterViewModel
+    {
+        [Required, EmailAddress]
+        public string Email { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        public string Password { get; set; }
+
+        [Required, DataType(DataType.Password)]
+        [Display(Name = "Confirm Password")] //display this instead of ConfirmPassword in one word.
+        public string ConfirmPassword { get; set; }
+    }
+}
