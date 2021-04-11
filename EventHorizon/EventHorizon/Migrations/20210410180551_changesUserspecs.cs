af8f1c788993890bc98beea730a2189c448436dc
Martin W <martin.wilhelmsson@utb.ecutbildning.se>
2021-04-11 09:19:01 +0200

    Gitignore add

---
+++ EventHorizon/EventHorizon/Migrations/20210410180551_changesUserspecs.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EventHorizon.Migrations
+{
+    public partial class changesUserspecs : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Name",
+                table: "Attendee");
+
+            migrationBuilder.RenameColumn(
+                name: "Phone",
+                table: "Attendee",
+                newName: "Password");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Attendee",
+                newName: "Phone");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Name",
+                table: "Attendee",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
